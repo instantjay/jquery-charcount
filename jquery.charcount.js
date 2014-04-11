ccfaf1bf622f2ef1f48fc225e2a500444f83c714
Jay <john@jenseninnovations.se>
2014-04-12 05:40:13 +0800

    Added ability to override maxLength using an attribute on the input element, as well as ability to enforce maxLength not being exceeded.

--- jquery.charcount.js
+++ jquery.charcount.js
@@ -2,43 +2,68 @@
 
 (function ( $ ) {
     $.fn.charCount = function (options) {
+        function calculateCurrentLength(inputElement) {
+            return inputElement.val().length;
+        }
+
+        function assignEventHandlers(inputElement, outputElement) {
+            inputElement.on("keyup click", function(e) {
+                var currentLength = calculateCurrentLength(inputElement);
+
+                if(maxLength) {
+                    if (currentLength >= maxLength) {
+                        if(settings.enforceMaxLength) {
+                            // If pasting text into the input field, we might overshoot the maxLength by a lot
+                            var lengthDiff = maxLength - currentLength;
+
+                            if(lengthDiff < 0) {
+                                var newText = inputElement.val().slice(0, lengthDiff);
+                                inputElement.val(newText);
+                            }
+                        }
+
+                        outputElement.addClass("max-length-exceeded");
+                    }
+                    else {
+                        outputElement.removeClass("max-length-exceeded");
+                    }
+                }
+
+                $(".current-length", outputElement).text(calculateCurrentLength(inputElement));
+            });
+        }
 
         // Default options
         var settings = $.extend({
             maxLength: null,
+            enforceMaxLength: false,
             delimiter: "/"
         }, options);
 
         for(i=0; i < this.length; i++) {
             // Initial setup
-            var charCounter = $("[data-charcounter-for="+this[i].id+"]");
+            var inputElement = $(this[i]);
+            var counterOutputElement = $("[data-charcounter-for="+inputElement.attr('id')+"]");
 
-            charCounter.html("<span class='current-length'>0</span>");
+            // Determine maxLength for this very instance
+            var maxLength = settings.maxLength;
+            if(inputElement.attr("data-max-length")) { // "Does user wish to override settings for this element?"
+                maxLength = inputElement.attr("data-max-length");
+            }
 
-            if(settings.maxLength !== null) {
-                charCounter.append(settings.delimiter);
-                charCounter.append("<span class='max-length'>"+settings.maxLength+"</span>");
+            // Add mark-up to the output element
+            counterOutputElement.html("<span class='current-length'>0</span>");
+
+            if(maxLength !== null) {
+                counterOutputElement.append(settings.delimiter);
+                counterOutputElement.append("<span class='max-length'>"+maxLength+"</span>");
             }
 
             // Assign event bindings
-            var textFieldId = this[i].id;
+            assignEventHandlers(inputElement, counterOutputElement);
 
-            $("[id="+this[i].id+"]").on("keyup click", function(e) {
-                var currentLength = $("[id="+textFieldId+"]").val().length;
-
-                $(".current-length", charCounter).text(currentLength);
-
-                if(settings.maxLength) {
-                    if (currentLength > settings.maxLength) {
-                        charCounter.addClass("max-length-exceeded");
-                    }
-                    else {
-                        charCounter.removeClass("max-length-exceeded");
-                    }
-                }
-            });
-
-            this[i].click();
+            // Do first-time char count of element
+            inputElement.click(); // I wanna get rid of this hack-ish solution to init the counters
         }
 
         return this;
